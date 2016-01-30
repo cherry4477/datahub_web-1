c158d9f8b9571f09dbc43b6020ba6fe5ad7b535f
sabxuee <xueyasheng@gmail.com>
2016-01-30 11:37:55 +0800

    Revert "最热item"
    This reverts commit 22d1db033f21b86d4c9a865fc071a24a2a39f176.

--- webapp/js/itemDetails.js
+++ webapp/js/itemDetails.js
@@ -1301,95 +1301,87 @@ function cancel_buy(){
         });
     });
 }
-
-
-
-
 //the amount of like:star
-function subscription(itemName){
-    if($.cookie("token")!=null&&$.cookie("token")!="null"){
-        headerToken={Authorization:"Token "+$.cookie("token")};
-    }
+function subscription(){
     var starAmount = '';
         var repoName=getParam("repname");
+        var itemName=getParam("itemname");
              $.ajax({
                 url: ngUrl + "/star_stat/" + repoName + "/" + itemName,
                 type: "GET",
                 cache: false,
                 async: false,
                 dataType: 'json',
-                headers: headerToken,
+                //headers: {Authorization: "Token " + $.cookie("token")},
                 success: function (json) {
                     if(json.code == 0){
-                        starAmount = json.data.numstars;
+                        var len=json.data.items;
+                        for (i=0;i<len;i++){
+                            starAmount = json.data.numstars;
                         }
                     }
+                    return starAmount;
+                 }
             });
         return starAmount;
 }
 //the amount of purchase icon cart
-function purchase(itemName){
-    if($.cookie("token")!=null&&$.cookie("token")!="null"){
-        headerToken={Authorization:"Token "+$.cookie("token")};
-    }
+function purchase(){
     var purchaseAmount = '';
         var repoName=getParam("repname");
+        var itemName=getParam("itemname");
             $.ajax({
                 url: ngUrl+"/subscription_stat/"+repoName+"/"+itemName,
                 type: "GET",
                 cache:false,
                 async:false,
                 dataType:'json',
-                headers:headerToken,
+                //headers:{Authorization:"Token "+$.cookie("token")},
                 success:function(json){
                     if(json.code == 0){
                         //$(".content1_pullNumber span:nth-child(2)").text("pull:"+json.data.nummypulls);
                         purchaseAmount=json.data.numsubs;
                     }
+                    return purchaseAmount;
                  }
              });
     return purchaseAmount;
  }
 //the amount of download the icon download
- function download_icon(itemName){
-     if($.cookie("token")!=null&&$.cookie("token")!="null"){
-         headerToken={Authorization:"Token "+$.cookie("token")};
-     }
+ function download_icon(){
      var downloadAmount ='';
      var repoName=getParam("repname");
+     var itemName=getParam("itemname");
      $.ajax({
          url: ngUrl+"/transaction_stat/"+repoName+"/"+itemName,
          type: "GET",
          cache:false,
          async:false,
          dataType:'json',
-         headers:headerToken,
+         //headers:{Authorization:"Token "+$.cookie("token")},
          success:function(json){
              if(json.code == 0){
                  downloadAmount = json.data.numpulls;
-             }
+             }return downloadAmount;
          }
      });
      return downloadAmount;
 }
 //the amount of comment
-function getComment(itemName){
-    if($.cookie("token")!=null&&$.cookie("token")!="null"){
-        headerToken={Authorization:"Token "+$.cookie("token")};
-    }
+function getComment(){
     var commentAmount='';
     var repoName=getParam("repname");
+    var itemName=getParam("itemname");
     $.ajax({
          url: ngUrl+"/comment_stat/"+repoName+"/"+itemName,
          type: "GET",
          cache:false,
          async:false,
          dataType:'json',
-        headers:headerToken,
          success:function(json){
              if(json.code == 0){
                 commentAmount=json.data.numcomments;
-             }
+             }return commentAmount;
         }
     });
     return commentAmount;
@@ -1408,15 +1400,16 @@ function hot(){
                 cache:false,
                 async:false,
                 dataType:'json',
-                headers:headerToken,
+                //headers:headerToken,
                 success:function(json) {
                     var iname=json.data.dataitems;
+                    console.log(iname);
                     var len=json.data.items;
-                    for (i=0;i<len;i++){
-                     var pnum = purchase(iname[i]);
-                     var dnum = download_icon(iname[i]);
-                     var starnum = subscription(iname[i]);
-                     var commentnum = getComment(iname[i]);
+                     for (i=0;i<len;i++){
+                       var pnum = purchase();
+                       var dnum = download_icon();
+                       var starnum = subscription();
+                       var commentnum = getComment();
                         $place.append(""+
                             "<p ID='subtitle' style='padding-top: 20px; padding-bottom:25px; font-size:20px; font-weight: bold; color:#43609f; float:left'>"+iname[i]+"</p>"+
                             "<div ID='icons' style='float:left; margin-left:20px; margin-bottom:15px'>"+
@@ -1428,7 +1421,7 @@ function hot(){
                             +"</div>"
                             +"<div ID='comment' style='float: left; width: 45px; margin-bottom: 15px; margin-left: -23px;'>"+"<img src='images/selects/images_14.png' style='padding-right: 15px;'>"+"<span>"+commentnum+"</span>"
                             +"</div>"+"</div>"+"</div>");                            
-                    }
+                    }    
                 }
     });
 }
